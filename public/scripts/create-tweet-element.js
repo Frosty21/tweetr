0570f942d0a762f17f65abdd84843fd559486822
Frosty21 <nathan.froese21@gmail.com>
2017-02-23 15:33:17 -0800

    change tweet overflow & like hover

--- public/scripts/create-tweet-element.js
+++ public/scripts/create-tweet-element.js
@@ -18,10 +18,9 @@ const createTweetElement = function (tweetData) {
     const tweetAge = convertDate(tweetData.created_at);
     $footer.append($("<p>").addClass("tweet-age").html(tweetAge + ' days ago'));
 
-    $footer.append($("<i>").addClass("icon fa fa-retweet fa-lg").attr('aria-hidden', "true"));
-    $footer.append($("<i>").addClass("icon fa fa-flag fa-lg").attr('aria-hidden', "true"));
     $footer.append($("<i>").addClass("icon fa fa-heart fa-lg").attr('aria-hidden', "true"));
-
+    $footer.append($("<i>").addClass("icon fa fa-flag fa-lg").attr('aria-hidden', "true"));
+    $footer.append($("<i>").addClass("icon fa fa-retweet fa-lg").attr('aria-hidden', "true"));
     $tweet.append($footer);
 
     return $tweet;
